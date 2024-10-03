8adc53432af2929d819a5ce2df1fc6a21d842e54
SteTessari <stephanietessare@gmail.com>
2024-10-03 17:40:38 -0300

    commit inicial

---
+++ src/main/java/com/example/api_processos_juridicos/ApiProcessosJuridicosApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_processos_juridicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProcessosJuridicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProcessosJuridicosApplication.class, args);
+	}
+
+}
