abfc542ff3fd65fd9cafcfc13b7e83f53aa59f42
SteTessari <stephanietessare@gmail.com>
2024-10-04 10:44:53 -0300

    Refactor: Adicionada validações do DTO ao inserir um processo jurídico

--- src/main/java/com/example/api_processos_juridicos/domain/processo/ProcessoJuridicoDTO.java
+++ src/main/java/com/example/api_processos_juridicos/domain/processo/ProcessoJuridicoDTO.java
@@ -2,6 +2,9 @@ package com.example.api_processos_juridicos.domain.processo;
 
 import com.example.api_processos_juridicos.domain.usuario.Usuario;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,14 +20,18 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProcessoJuridicoDTO implements Serializable {
-
+    @NotBlank(message = "Por favor informe o número do processo")
     private String numeroProcesso;
 
+    @NotNull(message = "Por favor informe a data de abertura")
     private LocalDate dataAbertura;
 
+    @NotBlank(message = "Por favor informe a descrição do caso")
     private String descricaoCaso;
 
+    @NotNull(message = "Por favor informe o status do processo")
     private StatusProcesso status;
 
+    @NotEmpty(message = "Por favor informe as partes envolvidas")
     private List<Usuario> partesEnvolvidas = new ArrayList<>();
 }
