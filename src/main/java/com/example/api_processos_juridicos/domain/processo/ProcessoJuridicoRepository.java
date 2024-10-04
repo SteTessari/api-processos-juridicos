cd290a7264a1fd1998250deaba1ffd74fd66ea1e
SteTessari <stephanietessare@gmail.com>
2024-10-04 17:28:07 -0300

    Feat: adicionada consultas

--- src/main/java/com/example/api_processos_juridicos/domain/processo/ProcessoJuridicoRepository.java
+++ src/main/java/com/example/api_processos_juridicos/domain/processo/ProcessoJuridicoRepository.java
@@ -3,12 +3,17 @@ package com.example.api_processos_juridicos.domain.processo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProcessoJuridicoRepository extends JpaRepository<ProcessoJuridico, Long> {
+    List<ProcessoJuridico> findByPartesEnvolvidas_InscricaoFederalIn(Collection<String> inscricaoFederals);
+    List<ProcessoJuridico> findByDataAberturaGreaterThanEqual(LocalDate dataAbertura);
     Optional<ProcessoJuridico> findByNumeroProcesso(String numeroProcesso);
 
     List<ProcessoJuridico> findByStatus(StatusProcesso status);
+
 }
