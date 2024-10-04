c706a01fac72e9a1e653c13e3e25d43554677dad
SteTessari <stephanietessare@gmail.com>
2024-10-04 17:28:33 -0300

    Feat: adicionado Controller de ProcessoJuridico

---
+++ src/main/java/com/example/api_processos_juridicos/controllers/usuario/ProcessoJuridicoController.java
@@ -0,0 +1,63 @@
+package com.example.api_processos_juridicos.controllers.usuario;
+
+import com.example.api_processos_juridicos.domain.pessoa.Pessoa;
+import com.example.api_processos_juridicos.domain.pessoa.PessoaService;
+import com.example.api_processos_juridicos.domain.processo.ProcessoJuridicoService;
+import com.example.api_processos_juridicos.domain.processo.StatusProcesso;
+import com.example.api_processos_juridicos.dto.pessoa.PessoaDTO;
+import com.example.api_processos_juridicos.dto.processo.ProcessoJuridicoDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/processos-juridicos")
+public class ProcessoJuridicoController {
+
+    @Autowired
+    private ProcessoJuridicoService processoJuridicoService;
+    @Autowired
+    private PessoaService pessoaService;
+
+
+    @PostMapping
+    public ProcessoJuridicoDTO criarProcesso(@Valid @RequestBody ProcessoJuridicoDTO processo) {
+        List<Pessoa> partesEnvolvidas = new ArrayList<>();
+
+        for (PessoaDTO parteEnvolvida : processo.getPartesEnvolvidas()) {
+            partesEnvolvidas.add(pessoaService.criar(parteEnvolvida));
+        }
+
+        return processoJuridicoService.criarProcesso(processo, partesEnvolvidas);
+    }
+
+    @GetMapping("/buscar/{numero}")
+    public ProcessoJuridicoDTO buscarProcesso(@PathVariable String numero) {
+        return processoJuridicoService.buscarProcessoDTOPorNumero(numero);
+    }
+
+    @PostMapping("/{numeroProcesso}")
+    public ProcessoJuridicoDTO editarProcesso(@PathVariable String numeroProcesso, @RequestBody ProcessoJuridicoDTO processo) {
+        return processoJuridicoService.editarProcesso(numeroProcesso, processo);
+    }
+
+    @GetMapping("/buscar/por-status")
+    public List<ProcessoJuridicoDTO> buscarPorStatus(@RequestParam StatusProcesso statusProcesso) {
+        return processoJuridicoService.listarProcessosPorStatus(statusProcesso);
+    }
+
+    @GetMapping("/buscar/por-data-abertura")
+    public List<ProcessoJuridicoDTO> buscarPorDataDeAbertura(@RequestParam LocalDate dataAbertura) {
+        return processoJuridicoService.listarProcessosPorDataAbertura(dataAbertura);
+    }
+
+    @PostMapping("/buscar/por-inscricao-federal")
+    public List<ProcessoJuridicoDTO> buscarPorInscricaoFederalDasPartes(@RequestBody List<String> inscricoesFederais) {
+        return processoJuridicoService.listarProcessosPorInscricaoFederalDasPartes(inscricoesFederais);
+    }
+
+}
