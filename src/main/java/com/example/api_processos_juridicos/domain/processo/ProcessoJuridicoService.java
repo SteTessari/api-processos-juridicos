d34f0f15a65330215a163dfd71e7ad07f7f05895
SteTessari <stephanietessare@gmail.com>
2024-10-04 11:18:38 -0300

    Feat: Criado método para editar processo pelo número

--- src/main/java/com/example/api_processos_juridicos/domain/processo/ProcessoJuridicoService.java
+++ src/main/java/com/example/api_processos_juridicos/domain/processo/ProcessoJuridicoService.java
@@ -14,11 +14,10 @@ public class ProcessoJuridicoService {
 
     @Autowired
     private ProcessoJuridicoRepository processoJuridicoRepository;
-
     private final ProcessoJuridicoMapper processoJuridicoMapper = ProcessoJuridicoMapper.INSTANCE;
 
     public ProcessoJuridicoDTO criarProcesso(ProcessoJuridicoDTO processo) {
-        buscarProcessoPorNumero(processo.getNumeroProcesso());
+        buscarProcessoDTOPorNumero(processo.getNumeroProcesso());
 
         ProcessoJuridico processoJuridico = processoJuridicoMapper.toObject(processo);
         processoJuridico = processoJuridicoRepository.save(processoJuridico);
@@ -26,7 +25,7 @@ public class ProcessoJuridicoService {
         return processoJuridicoMapper.toDTO(processoJuridico);
     }
 
-    public ProcessoJuridicoDTO buscarProcessoPorNumero(String numeroProcesso) {
+    public ProcessoJuridicoDTO buscarProcessoDTOPorNumero(String numeroProcesso) {
         Optional<ProcessoJuridico> processoJuridicoOptional = processoJuridicoRepository.findByNumeroProcesso(numeroProcesso);
 
         if (processoJuridicoOptional.isEmpty())
@@ -34,9 +33,26 @@ public class ProcessoJuridicoService {
 
         return processoJuridicoMapper.toDTO(processoJuridicoOptional.get());
     }
+    private ProcessoJuridico buscarProcessoPorNumero(String numeroProcesso) {
+        Optional<ProcessoJuridico> processoJuridicoOptional = processoJuridicoRepository.findByNumeroProcesso(numeroProcesso);
+
+        if (processoJuridicoOptional.isEmpty())
+            throw new ApiException(HttpStatus.NO_CONTENT, "Nenhum processo encontrado.");
+
+        return processoJuridicoOptional.get();
+    }
 
     public List<ProcessoJuridicoDTO> listarProcessosPorStatus(StatusProcesso status) {
         List<ProcessoJuridico> processoJuridicoList = processoJuridicoRepository.findByStatus(status);
         return processoJuridicoList.stream().map(processoJuridicoMapper::toDTO).collect(Collectors.toList());
+    }
+
+    public ProcessoJuridicoDTO editarProcesso(String numeroProcesso, ProcessoJuridicoDTO processoJuridicoAtualizadoDTO) {
+        ProcessoJuridico processoJuridico = buscarProcessoPorNumero(numeroProcesso);
+
+        ProcessoJuridico processoJuridicoAtualizado = processoJuridicoMapper.updateFromDTO(processoJuridicoAtualizadoDTO, processoJuridico);
+        processoJuridicoAtualizado = processoJuridicoRepository.save(processoJuridicoAtualizado);
+
+        return processoJuridicoMapper.toDTO(processoJuridicoAtualizado);
     }
 }
